f92bb94c7b7b90aaa02d23c915b770aec44a1b01
Pedro Pivato <pivatopedro@gmail.com>
2026-03-03 19:58:41 -0300

    adicionado page objects para testes de cadastro

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,24 +1,26 @@
 /// <reference types="cypress"/>
 import { faker } from '@faker-js/faker';
+import cadastroPage from '../support/pages/cadastro-page';
+
 const nome = faker.person.fullName()
 const email = faker.internet.email()
 const senha = faker.internet.password()
 const telefone = faker.phone.number()
 
 beforeEach(() => {
-    cy.visit('register.html')
+    cadastroPage.visitarPaginaCadastro()
 });
 
 describe('Funcionalidade: Cadastro no Hub de Leitura', () => {
-    it('Deve fazer cadastro com sucesso', () => {
+    it.skip('Deve fazer cadastro com sucesso', () => {
         cy.get('#name').type('Pedro Pivato')
-        cy.get('#email').type('pedro3@teste.com')
+        cy.get('#email').type('pedro4@teste.com')
         cy.get('#phone').type('996517201')
         cy.get('#password').type('senha123')
         cy.get('#confirm-password').type('senha123')
         cy.get('#terms-agreement').check()
         cy.get('#register-btn').click()
-        cy.url().should * ('include', 'dashboard')
+        cy.url().should('include', 'dashboard')
         cy.get('#user-name').should('contain', 'Pedro')
 
     });
@@ -33,7 +35,7 @@ describe('Funcionalidade: Cadastro no Hub de Leitura usando Faker', () => {
         cy.get('#confirm-password').type(senha)
         cy.get('#terms-agreement').check()
         cy.get('#register-btn').click()
-        cy.url().should * ('include', 'dashboard')
+        cy.url().should('include', 'dashboard')
         cy.get('#user-name').should('contain', nome)
 
     });
@@ -47,5 +49,17 @@ describe('Funcionalidade: Cadastro no Hub de Leitura usando Faker', () => {
             senha
         )
         cy.url().should('contain', 'dashboard.html')
+    });
+});
+
+describe('Funcionalidade: Cadastro com Page Objects', () => {
+    it('Deve fazer cadastro com sucesso - usando Page Objects', () => {
+        cadastroPage.preencherCadastro( nome, email,telefone,senha,senha)
+        cy.url().should('contain', 'dashboard.html')
+    });
+
+    it.only('Deve validar mensagem ao tentar cadastrar sem preencher nome', () => {
+        cadastroPage.preencherCadastro( '', email,telefone,senha,senha)
+        cy.get('.invalid-feedback').should('contain', 'Nome deve ter pelo menos 2 caracteres')
     });
 });
